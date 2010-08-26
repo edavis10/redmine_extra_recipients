d9197a57fca20d91bd95c3afa0647a62f2e3a723
Eric Davis <edavis@littlestreamsoftware.com>
2010-08-26 10:22:25 -0700

    [#4439] Generated documentation.

--- init.rb
+++ init.rb
@@ -1,10 +1,12 @@
 require 'redmine'
 
 Redmine::Plugin.register :redmine_extra_recipients do
-  name 'Redmine Extra Recipients plugin'
-  author 'Author name'
-  description 'This is a plugin for Redmine'
-  version '0.0.1'
-  url 'http://example.com/path/to/plugin'
-  author_url 'http://example.com/about'
+  name 'Redmine Extra Recipients'
+  author 'Eric Davis'
+  description 'Adds additional recipients to email notifications'
+  url 'https://projects.littlestreamsoftware.com/projects/redmine-recipients'
+  author_url 'http://www.littlestreamsoftware.com'
+  version '0.1.0'
+
+  requires_redmine :version_or_higher => '1.0.0'
 end
