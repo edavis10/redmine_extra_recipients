9637285151eefd5c84043d1b23b29caf84a9e859
Eric Davis <edavis@littlestreamsoftware.com>
2010-08-26 10:17:29 -0700

    [#4439] Generated plugin.

---
+++ init.rb
@@ -0,0 +1,10 @@
+require 'redmine'
+
+Redmine::Plugin.register :redmine_extra_recipients do
+  name 'Redmine Extra Recipients plugin'
+  author 'Author name'
+  description 'This is a plugin for Redmine'
+  version '0.0.1'
+  url 'http://example.com/path/to/plugin'
+  author_url 'http://example.com/about'
+end
