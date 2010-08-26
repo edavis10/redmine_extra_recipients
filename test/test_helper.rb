313f9a09e1dc4147b783d8fc9a944260a17691e2
Eric Davis <edavis@littlestreamsoftware.com>
2010-08-26 10:39:07 -0700

    [#4439] Updated the test_helper.rb with new helpers.

--- test/test_helper.rb
+++ test/test_helper.rb
@@ -3,3 +3,64 @@ require File.expand_path(File.dirname(__FILE__) + '/../../../../test/test_helper
 
 # Ensure that we are using the temporary fixture path
 Engines::Testing.set_fixture_path
+
+require "webrat"
+
+Webrat.configure do |config|
+  config.mode = :rails
+end
+
+def User.add_to_project(user, project, role)
+  Member.generate!(:principal => user, :project => project, :roles => [role])
+end
+
+module RedmineWebratHelper
+  def login_as(user="existing", password="existing")
+    visit "/login"
+    fill_in 'Login', :with => user
+    fill_in 'Password', :with => password
+    click_button 'login'
+    assert_response :success
+    assert User.current.logged?
+  end
+
+  def visit_project(project)
+    visit '/'
+    assert_response :success
+
+    click_link 'Projects'
+    assert_response :success
+
+    click_link project.name
+    assert_response :success
+  end
+
+  def visit_issue_page(issue)
+    visit '/issues/' + issue.id.to_s
+  end
+
+  def visit_issue_bulk_edit_page(issues)
+    visit url_for(:controller => 'issues', :action => 'bulk_edit', :ids => issues.collect(&:id))
+  end
+end
+
+class ActionController::IntegrationTest
+  include RedmineWebrateHelper
+end
+
+class ActiveSupport::TestCase
+  def assert_forbidden
+    assert_response :forbidden
+    assert_template 'common/403'
+  end
+
+  def configure_plugin(configuration_change={})
+    Setting.plugin_redmine_extra_recipients = {
+      
+    }.merge(configuration_change)
+  end
+
+  def reconfigure_plugin(configuration_change)
+    Settings['plugin_redmine_extra_recipients'] = Setting['plugin_redmine_extra_recipients'].merge(configuration_change)
+  end
+end
